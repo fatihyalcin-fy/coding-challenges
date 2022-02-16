31661038ed44f7eeb82ab8e7d2f158764a0f40a2
fatihyalcn <fatihyalcn9332@gmail.com>
2022-02-16 11:28:55 +0100

    changes-done

--- email-validation/app.js
+++ email-validation/app.js
@@ -1,4 +1,4 @@
-const eMail = document.getElementById("email");
+/* const eMail = document.getElementById("email");
 const buttonCheck = document.querySelector(".button");
 const result = document.querySelector(".result")
 
@@ -9,4 +9,29 @@ buttonCheck.addEventListener("click", ()=>{
 
     else if(eMail.value[-3] == "." || eMail);
 
-});
+}); */
+
+//üç sayı ortalaması alma
+
+/* let number1 = prompt("İlk sayı: ");
+let number2 = prompt("İlk sayı: ");
+let number3 = prompt("İlk sayı: ");
+
+let result = (Number(number1) + Number(number2) + Number(number3)) / 3
+
+console.log(result); */
+
+//üçgenin cinsini veren fonksiyon
+
+let birinciKenar = parseInt(prompt("Birinci kenarın uzunluğunu giriniz: "))
+let ikinciKenar = parseInt(prompt("İkinci kenarın uzunluğunu giriniz: "))
+let ucuncuKenar = parseInt(prompt("Üçüncü kenarın uzunluğunu giriniz: "))
+
+
+if((birinciKenar == ikinciKenar) && (birinciKenar == ucuncuKenar)){
+    alert("Eşkenar");
+}else if(birinciKenar != ikinciKenar && birinciKenar != ucuncuKenar && ikinciKenar != ucuncuKenar){
+    alert("Çeşitkenar");
+}else{
+    alert("İkiz kenar");
+}
