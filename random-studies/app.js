f25e7dc44ee84d28ef4bcd0d48f7ac1be80fdb5b
fatihyalcn <fatihyalcn9332@gmail.com>
2022-02-16 11:57:54 +0100

    updt

---
+++ random-studies/app.js
@@ -0,0 +1,25 @@
+// const age = "11"
+
+// if(age >=12){
+//     console.log(`Your age is ${age}`);
+// }else{
+//     console.log(`Your age is ${age} and you are too young`);
+// }
+
+// let category = "araba";
+
+// switch(category){
+//     case 'telefon' :
+//         console.log('telefon kategorisi');
+//     break;
+
+//     case 'bilgisayar' :
+//         console.log("pc category");
+//     break;
+
+//     default:
+//         console.log('kategori yok');
+// }
+
+
+let trafikCikis = new Date('04/20/2016');
